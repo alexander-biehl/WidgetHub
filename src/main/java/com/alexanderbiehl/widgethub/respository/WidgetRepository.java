1af0b17621e7ca24ae07ddf6ce81ea6187445367
alexander-biehl <biehl.al@gmail.com>
2024-04-11 12:09:59 -0400

    added basic repository

---
+++ src/main/java/com/alexanderbiehl/widgethub/respository/WidgetRepository.java
@@ -0,0 +1,14 @@
+package com.alexanderbiehl.widgethub.respository;
+
+import com.alexanderbiehl.widgethub.model.Widget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface WidgetRepository extends JpaRepository<Widget, Long> {
+
+    List<Widget> findbyName(@Param("name") String name);
+}
